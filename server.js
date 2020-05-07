f5933899efb00a9910c9838d88220e6f3e408c38
Garrett <garrettgassensmith@gmail.com>
2020-05-07 15:16:13 -0500

    Built server

---
+++ server.js
@@ -0,0 +1,38 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// ******************************************************************************
+
+// Dependencies
+// =============================================================
+const express = require("express");
+const exphbs = require("express-handlebars");
+const db = require("./models");
+
+// Setup Express
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Express - data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Express - static directory
+app.use(express.static("public"));
+
+// Express - config Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+// =============================================================
+require("./controllers/html-routes.js")(app);
+require("./controllers/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
