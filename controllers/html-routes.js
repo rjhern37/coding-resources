4097776f76a9d5d00b6d7050883d73046b2111dc
Garrett <garrettgassensmith@gmail.com>
2020-05-12 09:08:45 -0500

    Created html routes

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -4,21 +4,30 @@
 
 // Dependencies
 // =============================================================
-var path = require("path");
+const path = require("path");
+const authenitcate = require("../config/authenticate");
+
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
-  app.get("/", function(req, res) {
-    res.render("index");
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("index");
+    }
+    res.render("signup");
   });
+
+  app.get("/login", function (req, res) {
+    if (req.user) {
+      res.render("index");
+    }
+    res.render("login");
+  });
+
+  app.get("/home", function (req, res) {
+      res.render("index");
+  });
+
 };
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(_dirname, "..public/script.html"));
-});
-
-app.get("/line", function(req, res) {
-  res.sendFile(path.join(_dirname, "../public/line-manager.html"))
-});
