9c3f87ee8b4b85e3c4d61df38bae581628286c39
Garrett <garrettgassensmith@gmail.com>
2020-05-14 17:39:42 -0500

    Created a create tag button in the create resource form

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   const createBtn = $("#create-resource");
   const searchBtn = $("#search-btn");
   const addTagBtn = $("#add-tag-btn");
+  const createTagBtn = $("#create-tag-btn");
   const usernameInput = $("#username-input");
   const passwordInput = $("#password-input");
   const nameInput = $("#name-input");
@@ -11,6 +12,7 @@ $(document).ready(function () {
   const descriptionInput = $("#description-input");
   const tagSelect = $("#tag-select");
   const addedTagList = $("#added-tag-list");
+  const createTagInput = $("#create-tag-input");
 
 
   // USER
@@ -77,7 +79,7 @@ $(document).ready(function () {
   // ==================================================
   // Create new resource
   function createResource(name, description, link) {
-    $.post("/api/create", {
+    $.post("/api/resources/create", {
       resourceName: name,
       description: description,
       link: link,
@@ -155,93 +157,23 @@ $(document).ready(function () {
   // TAGS
   // ==================================================
   // Create tag 
-  function createTag(tagId, tagName) {
-    let tag = `<button class="button" data-id="${tagId}">${tagName}</button>`;
-    addedTagList.append(tag);
+  function createTag(tagName) {
+    $.post("/api/tags/create", {
+      tagName: tagName
+    })
+      .then(function () {
+        window.location.replace("/create");
+
+        // If there's an error, log the error
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
-  addTagBtn.on("click", function (e) {
+  createTagBtn.on("click", function (e) {
     e.preventDefault();
-    let tagId = tagSelect.val();
-    let tagName = $("#tag-select option:selected").text();
-    addTag(tagId, tagName);
+    let tagName = createTagInput.val().trim();
+    createTag(tagName);
   });
 
-  
-
-  // var nameInput = $("#line-name")
-  // //adding more var
-
-  // $(document).on("submit", ".line-form", handleLineFormSubmit);
-  // $(document).on("click", ".delete-line", handleDeleteButtonPress);
-
-  // getLine();
-
-  // function handleLineFormSubmit(event) {
-  //     event.preventDefault();
-  //     //if
-  // }
-
-  // upsertLine({
-  //     name: nameInput
-  //     .val()
-  //     .trim()
-  // });
-
-  // function infoData(userData) {
-  //     $.post("/api/info", userData)
-  //     .then(getInfo);
-  // }
-
-  // function createInfoRow(userData) {
-  //     var newTr = $("<tr>");
-  //     newTr.data("<td>" + userData);
-  //     newTr.append("<td>" + userData.name + "</td>");
-  //     if (userData.Posts) {
-  //         newTr.append("<td>" + userData.name + "</td>");
-  //     } else {
-  //         newTr.append("<td>0</td>");
-
-  //     }
-  //     return newTr;
-  // }
-
-  // function getLine() {
-  //     $.get("/api/Line", function(data) {
-  //         var rowsToAdd = [];
-  //         for (var i = 0; i <data.length; i++) {
-  //             rowsToAdd.push(creatLineRow(data[i]));
-  //         }
-  //         renderLineList(rowsToAdd);
-  //         nameInput.Val("");
-  //     });
-  // }
-
-  // function renderLineList(rows) {
-  //     authorList.children().not(":list").remove();
-  //     authorContainer.children(".alert").remove();
-  //     if (rows.length) {
-  //         console.long(rows);
-  //         authorList.prepend(rows);
-  //     }
-  //     else {
-  //         renderEmpty();
-  //     }
-  // }
-
-  // function renderEmpty() {
-  //     var alertDiv = $("<div>");
-  //     alertDiv.addClass("alert alert danger");
-  //     alertDiv.text("You must create a line before you can make a post.")
-  //     authorContainer.append(alertDiv);
-  // }
-
-  // function handleDeleteButtonPress() {
-  //     var listItemData = $(this).parent("td").parent("tr").data("author");
-  //     var id = listItemData.id;
-  //     $.ajax({
-  //         method: "DELETE",
-  //         url: "/api/line/" + id
-  //     })
-  //     .then(getLine);
-  // }
 });
